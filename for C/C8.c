49a147360250ce16e7dd8836acf26f35707ceec5
VachugovaK <vachugova.katya@mail.ru>
2017-05-09 17:48:10 +0300

    Update C8.c

--- for C/C8.c
+++ for C/C8.c
@@ -10,16 +10,19 @@ int main()
 	scanf("%f %f %f", &a, &b, &c);
 	D = (b*b) - (4 * a * c);
 	float x1, x2;
-	if (D > 0)
+	if (D >= 0)
 	{
+		if (D == 0)
+		{
+		x1 = x2 = (-b / (2 * a));
+		printf("x1 = x2 = %f", x1);
+		}
+		else 
+		{
 		x1 = (-b + sqrt(D)) / (2 * a);
 		x2 = (-b - sqrt(D)) / (2 * a);
 		printf("x1 = %f, x2 = %f", x1, x2);
-	}
-	else if (D == 0)
-	{
-		x1 = x2 = (-b / (2 * a));
-		printf("x1 = x2 = %f", x1);
+		}
 	}
 	else
 	{
