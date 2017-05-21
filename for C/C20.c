9fb56d68c98af1e37183a5d6ff0ed64d2d5a18fa
VachugovaK <vachugova.katya@mail.ru>
2017-05-21 18:12:11 +0300

    Create C20.c

---
+++ for C/C20.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+FILE *X;
+FILE *C;
+void XOR(char *str,char *key )
+{
+    int i,n;
+    for(i=0;i<strlen(str);++i)
+    {
+        if(str[i]!=EOF)
+        {
+            n=i%strlen(key);
+            str[i]^=key[n];
+            fputc(str[i],X);
+        }
+    }
+
+}
+int main()
+{
+    setlocale(LC_ALL,"Russian");
+    char stroka[10000],m,s,key[100],fail[100];
+    int i=0,j;
+    printf("Введите ключ: ");
+    gets(key);
+    printf("Введите путь к вашему файлу (По типу: C:/Papka/file.xls): ";
+    gets(fail);
+    C=fopen(fail,"r");
+    while(!feof(C))
+    {
+        stroka[i]=fgetc(C);
+        i++;
+    }
+    fclose(C);
+    X=fopen(fail,"w");
+    XOR(stroka,key);
+    printf("Ваш текст закодирован");
+    return 0;
+}
